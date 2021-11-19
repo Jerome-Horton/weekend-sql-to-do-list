5a32324f5cad72b5e95dd336a1db2a747e512109
Jerome-Horton <riqui.howard@gmail.com>
2021-11-19 14:02:21 -0600

    Created To-Do-List, Created folders & files, and Linked all files

---
+++ server/modules/pool.js
@@ -0,0 +1,22 @@
+// Require the pg module:
+const pg = require('pg');
+
+// Create a pool object constructor.
+const Pool = pg.Pool;
+
+// Create pool object using the above constructor:
+const pool = new Pool({
+    database: 'weekend-to-do-app', // the name of database, This can change!
+    host: 'localhost' // where is your database?
+});
+
+// Check console when our pool object makes a connection:
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+});
+
+// Check console when something makes our pool error out:
+pool.on('error', (error) => {
+    console.log('Error with postgres pool', error)
+});
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const toDoListRouter = require('./routes/toDoListRouter')
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('server/public'));
+
+// Routes
+app.use( '/toDoList', toDoListRouter);
+
+// Starts the server, and listens for requests:
+app.listen(PORT, () => {
+  console.log('listening on port', PORT)
+});
